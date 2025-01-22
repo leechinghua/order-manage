513d7ece996ca5b94f40b8b2445a4f4ad4f6d5f6
leechinghua <leechenghua107@gmail.com>
2025-01-22 13:17:44 +0800

    設定主題色

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,16 +1,24 @@
 import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
     palette: {
+      mode: 'light',
       primary: {
-        main: '#144469', // 主色 (藍色)
+        main: '#F3F4F6', // 灰
+        dark: '#D1D5DB',
       },
       secondary: {
-        main: '#C7E4FC', // 次色 (淺藍色)
+        main: '#C7E4FC', // 淺藍色
+        dark: '#28A9DF',
       },
+      text: {
+        primary: '#144469',
+        secondary: '#3A4F66',
+    },
     },
     typography: {
       fontFamily: 'Roboto, Arial, sans-serif',
     },
+    
   });
   
   export default theme;
